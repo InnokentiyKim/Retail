624489b0ac070bde433696424e37e925a7a5580b
inncent <innokentiykim90@gmail.com>
2025-01-21 23:04:11 +0500

    models parameters changed

--- backend/models.py
+++ backend/models.py
@@ -22,7 +22,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    description = models.TextField(blank=True, nullable=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -32,7 +32,7 @@ class ProductDetails(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='details')
     quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(1000)])
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    parameters = models.JSONField(default=dict, blank=True, nullable=True)
+    parameters = models.JSONField(default=dict, blank=True, null=True)
 
 
 class Contact(models.Model):
