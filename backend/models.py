75b74b893f027300b90a3016dd6549888a599676
inncent <innokentiykim90@gmail.com>
2025-01-22 23:44:59 +0500

    Models updated

--- backend/models.py
+++ backend/models.py
@@ -22,7 +22,7 @@ class Shop(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField(max_length=300, blank=True, null=True)
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
-
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -35,26 +35,41 @@ class Shop(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    shops = models.ManyToManyField(Shop, blank=True, related_name='categories')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = ''
+        verbose_name_plural = ''
+        ordering = ['-name']
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     description = models.TextField(blank=True, null=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Список продуктов'
+        ordering = ['-name']
+
 
 class ProductDetails(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='details')
-    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(1000)])
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10000)])
+    price = models.DecimalField(max_digits=10, decimal_places=2, positive=True)
+    price_retail = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, positive=True)
     parameters = models.JSONField(default=dict, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Описание продукта'
+        verbose_name_plural = 'Описание продуктов'
 
 
 class Contact(models.Model):
@@ -69,7 +84,11 @@ class Contact(models.Model):
     apartment = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
-        return self.phone
+        return f"{self.city}, {self.street}, {self.house}. Phone number is {self.phone}"
+
+    class Meta:
+        verbose_name = 'Контакты пользователя'
+        verbose_name_plural = 'Список контактов'
 
 
 class Order(models.Model):
@@ -84,10 +103,22 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     created_at = models.DateTimeField(auto_now_add=True)
     state = models.CharField(max_length=12, choices=OrderStateChoices.choices, default=OrderStateChoices.new)
-    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, blank=True, null=True, related_name='orders')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, blank=True, related_name='orders')
+
+    def __str__(self):
+        return f"{self.created_at} - {self.state}"
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Список заказов'
+        ordering = ['-created_at']
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
     quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = 'Позиция заказа'
+        verbose_name_plural = 'Список позиций заказа'
