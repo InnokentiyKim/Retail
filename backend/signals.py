3a502df545018f615b360ab09c323c9b0b8232f5
inncent <innokentiykim90@gmail.com>
2025-01-30 22:16:04 +0500

    signals added

---
+++ backend/signals.py
@@ -0,0 +1,46 @@
+from typing import Type
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import post_save
+from django.dispatch import receiver, Signal
+from django_rest_passwordreset.signals import reset_password_token_created
+
+from backend.models import User, EmailTokenConfirm, Order
+
+new_user_registered = Signal()
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    msg = EmailMultiAlternatives(
+        "Password reset token",
+        reset_password_token.key,
+        settings.EMAIL_HOST_USER,
+        [reset_password_token.email]
+    )
+    msg.send()
+
+
+@receiver(post_save, sender=User)
+def new_user_registered_signal(sender: Type[User], instance: User, created: bool, **kwargs):
+    if created and not instance.is_active:
+        token, _ = EmailTokenConfirm.objects.get_or_create(user=instance)
+        msg = EmailMultiAlternatives(
+            "Password reset token",
+            token.key,
+            settings.EMAIL_HOST_USER,
+            [instance.email]
+        )
+        msg.send()
+
+
+@receiver(new_user_registered)
+def new_order_signal(user_id, **kwargs):
+    user = User.objects.get(pk=user_id)
+    msg = EmailMultiAlternatives(
+        "Обновление статуса заказа",
+        "Заказ сформирован",
+        settings.EMAIL_HOST_USER,
+        [user.email]
+    )
+    msg.send()
