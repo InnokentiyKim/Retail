c776d1e2821dfeb87f8813060e2de7f7fee09620
inncent <innokentiykim90@gmail.com>
2025-02-02 23:16:12 +0500

    Serializers modified

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,4 +1,4 @@
-from backend.models import ProductDetails, Contact, Order, OrderItem
+from backend.models import ProductDetails, Contact, Order, OrderItem, Property, ProductProperty
 from models import User, Shop, Category, Product
 from rest_framework import serializers
 
@@ -41,15 +41,35 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'description', 'shop']
+        fields = ['id', 'name', 'category']
+        read_only_fields = ['id']
 
 
 class ProductDetailsSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
+    shop = ShopSerializer(read_only=True)
 
     class Meta:
         model = ProductDetails
-        fields = ['id', 'product', 'quantity', 'price', 'price_retail', 'parameters']
+        fields = ['id', 'product', 'shop', 'quantity', 'price', 'price_retail']
+        read_only_fields = ['id']
+
+
+class PropertySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Property
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
+class ProductPropertySerializer(serializers.ModelSerializer):
+    product_details = ProductDetailsSerializer(read_only=True)
+    property = PropertySerializer(read_only=True)
+
+    class Meta:
+        model = ProductProperty
+        fields = ['id', 'product_details', 'property', 'value']
         read_only_fields = ['id']
 
 
@@ -65,11 +85,11 @@ class OrderSerializer(serializers.ModelSerializer):
 
 class OrderItemSerializer(serializers.ModelSerializer):
     order = OrderSerializer(read_only=True)
-    product = ProductSerializer(read_only=True)
+    product_details = ProductDetailsSerializer(read_only=True)
 
     class Meta:
         model = OrderItem
-        fields = ['id', 'order', 'product', 'quantity']
+        fields = ['id', 'order', 'product_details', 'quantity']
         read_only_fields = ['id']
         extra_kwargs = {
             'order': {'read_only': True},
