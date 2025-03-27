446bbd1469c0e921d93971dfe7d1e26d3304809c
inncent <innokentiykim90@gmail.com>
2025-03-27 22:36:10 +0500

    Profile model and create_profile function added

--- backend/models.py
+++ backend/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.conf import settings
 import uuid
 
 
@@ -98,6 +99,15 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+class Profile(models.Model):
+    objects = UserManager()
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='images/users/%Y/%m/%d', blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.email}"
 
 
 class Shop(models.Model):
